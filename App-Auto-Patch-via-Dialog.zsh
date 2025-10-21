451846d4e1f8a763ff436b8dc9d9032768d4c757
maxsundellacne <max.sundell@acnestudios.com>
2025-10-21 10:21:49 +0200

    3.4.2-a1

--- App-Auto-Patch-via-Dialog.zsh
+++ App-Auto-Patch-via-Dialog.zsh
@@ -23,8 +23,8 @@
 # Script Version and Variables
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-scriptVersion="3.4.2"
-scriptDate="2025/10/20"
+scriptVersion="3.4.2-a1"
+scriptDate="2025/10/21"
 scriptFunctionalName="App Auto-Patch"
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
